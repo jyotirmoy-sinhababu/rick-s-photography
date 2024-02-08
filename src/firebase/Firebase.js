6d29189b865be179f81e4e4700096e147d902e8e
jyotirmoy sinhababu <jyotirmoysinhababu@gmail.com>
2024-02-08 12:17:02 +0530

    app created

---
+++ src/firebase/Firebase.js
@@ -0,0 +1,14 @@
+import { initializeApp } from 'firebase/app';
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyCgHfSthcZclyW4Ao55bpEUvL8W8QM6M_I',
+  authDomain: 'rick-s-photography.firebaseapp.com',
+  projectId: 'rick-s-photography',
+  storageBucket: 'rick-s-photography.appspot.com',
+  messagingSenderId: '262806171544',
+  appId: '1:262806171544:web:976a3269a8b5f00f35c5ed',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
